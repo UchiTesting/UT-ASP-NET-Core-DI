a7e587bbc1eee3f8d25f09a2c6b5e023ad14b852
UchiTesing <uchitesting57@gmail.com>
2022-06-11 17:57:10 +0200

    Created 2 types being injected the Operation service

---
+++ DependencyService1.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace ASP_DI;
+
+public class DependencyService1
+{
+    private readonly IOperationTransient transient;
+    private readonly IOperationScoped scoped;
+    private readonly IOperationSingleton singleton;
+    private readonly IOperationSingletonInstance singletonInstance;
+    private readonly IOutputLogger _outputLogger;
+
+    public DependencyService1(IOperationScoped operationScoped,
+            IOperationSingleton operationSingleton,
+            IOperationSingletonInstance operationSingletonInstance,
+            IOperationTransient operationTransient,
+            IOutputLogger outputLogger)
+    {
+        transient = operationTransient;
+        scoped = operationScoped;
+        singleton = operationSingleton;
+        singletonInstance = operationSingletonInstance;
+        _outputLogger = outputLogger;
+    }
+
+    public void Write()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        sb.AppendLine($"{Environment.NewLine}From Dependency Service 1");
+        sb.AppendLine($"Transient → {transient.OperationId}");
+        sb.AppendLine($"Scoped → {scoped.OperationId}");
+        sb.AppendLine($"Singleton → {singleton.OperationId}");
+        sb.AppendLine($"SingletonInstance → {singletonInstance.OperationId}");
+
+        _outputLogger.Log(sb.ToString());
+    }
+}
---
+++ DependencyService2.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace ASP_DI;
+
+public class DependencyService2
+{
+    private readonly IOperationTransient transient;
+    private readonly IOperationScoped scoped;
+    private readonly IOperationSingleton singleton;
+    private readonly IOperationSingletonInstance singletonInstance;
+    private readonly IOutputLogger _outputLogger;
+
+    public DependencyService2(IOperationScoped operationScoped,
+            IOperationSingleton operationSingleton,
+            IOperationSingletonInstance operationSingletonInstance,
+            IOperationTransient operationTransient,
+            IOutputLogger outputLogger)
+    {
+        transient = operationTransient;
+        scoped = operationScoped;
+        singleton = operationSingleton;
+        singletonInstance = operationSingletonInstance;
+        _outputLogger = outputLogger;
+
+    }
+
+    public void Write()
+    {
+        StringBuilder sb = new StringBuilder();
+
+        sb.AppendLine("From Dependency Service 2");
+        sb.AppendLine($"Transient → {transient.OperationId}");
+        sb.AppendLine($"Scoped → {scoped.OperationId}");
+        sb.AppendLine($"Singleton → {singleton.OperationId}");
+        sb.AppendLine($"SingletonInstance → {singletonInstance.OperationId}");
+
+        _outputLogger.Log(sb.ToString());
+    }
+
+}
