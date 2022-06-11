f26fa7d5ca614547b14619885244c9ef52804006
UchiTesing <uchitesting57@gmail.com>
2022-06-11 17:56:17 +0200

    Created an OutputLogger service to display in the standard output

---
+++ OutputLogger.cs
@@ -0,0 +1,18 @@
+﻿namespace ASP_DI;
+public class OutputLogger : IOutputLogger
+{
+    public void Log(string message)
+    {
+#if DEBUG
+        System.Diagnostics.Debug.WriteLine(message);
+#endif
+#if RELEASE
+        System.Diagnostics.Trace.WriteLine(message);
+#endif
+    }
+}
+
+public interface IOutputLogger
+{
+    void Log(string message);
+}
