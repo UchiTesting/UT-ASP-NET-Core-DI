5ce8cd5b76bc4e2a50b6052bf804522a0eb7956c
UchiTesing <uchitesting57@gmail.com>
2022-06-11 17:57:29 +0200

    Registering the services

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using ASP_DI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var item = new ServiceDescriptor(
+    typeof(IOperationTransient),
+    typeof(Operation),
+    ServiceLifetime.Transient);
+
+builder.Services.Add(item);
+
+builder.Services.AddTransient<IOperationTransient, Operation>();
+builder.Services.AddScoped<IOperationScoped, Operation>();
+builder.Services.AddSingleton<IOperationSingleton, Operation>();
+builder.Services.AddSingleton<IOperationSingletonInstance>(a => new Operation(Guid.Empty));
+builder.Services.AddSingleton<IOperationSingletonInstance>(a => new Operation());
+
+builder.Services.AddTransient<IOutputLogger, OutputLogger>();
+builder.Services.AddTransient<DependencyService1, DependencyService1>();
+builder.Services.AddTransient<DependencyService2, DependencyService2>();
 
 var app = builder.Build();
 
