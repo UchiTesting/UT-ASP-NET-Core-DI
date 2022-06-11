315b86ef200f7a88adb68edd6419f25778c0f607
UchiTesing <uchitesting57@gmail.com>
2022-06-11 17:55:33 +0200

    Created an Operation service for demo

---
+++ Dependency.cs
@@ -0,0 +1,24 @@
+﻿namespace ASP_DI
+{
+    public interface IOperation
+    {
+        Guid OperationId { get; }
+    }
+
+    public interface IOperationTransient : IOperation { }
+    public interface IOperationScoped : IOperation { }
+    public interface IOperationSingleton : IOperation { }
+    public interface IOperationSingletonInstance : IOperation { }
+
+    public class Operation : IOperationScoped, IOperationSingleton, IOperationSingletonInstance, IOperationTransient
+    {
+        public Operation() : this(Guid.NewGuid()) { }
+
+        public Operation(Guid id)
+        {
+            OperationId = id;
+        }
+
+        public Guid OperationId { get; private set; }
+    }
+}
